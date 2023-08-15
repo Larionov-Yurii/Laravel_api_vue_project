69b9db7071eecbb5e46999af185d45b5aa613082
Larionov-Yurii <larionov.yurii.work@gmail.com>
2023-08-15 22:03:41 +0300

    Updated app.js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import "./bootstrap";
+import "../css/app.css";
+import { createApp } from "vue/dist/vue.esm-bundler";
+import SearchForm from "./components/SearchForm.vue";
+
+const app = createApp({
+    components: {
+        "search-form": SearchForm,
+    },
+});
+
+app.mount("#app");
