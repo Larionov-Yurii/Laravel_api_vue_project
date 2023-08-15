f97652dc5227fc9bc9938deea280a6c706962803
Larionov-Yurii <larionov.yurii.work@gmail.com>
2023-08-15 22:05:44 +0300

    Created search-form.blade.php file

---
+++ resources/views/search-form.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your App Title</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div id="app">
+        <search-form></search-form>
+    </div>
+</body>
+</html>
