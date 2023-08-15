b719d28f6bc6f5ebfb6114959bfc6c0368ab1a1f
Larionov-Yurii <larionov.yurii.work@gmail.com>
2023-08-15 21:59:09 +0300

    Updated api.php file

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PropertyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('properties')->group(function () {
+    Route::get('', [PropertyController::class, 'search']);
+    Route::get('/attribute', [PropertyController::class, 'searchByAttribute']);
+    Route::get('/price/{priceRange}', [PropertyController::class, 'searchByPriceRange']);
 });
+
+/**
+ * Routes for all filters:
+ * http://localhost:8000/api/properties - get all records
+ * http://localhost:8000/api/properties?name=Victoria&bedrooms=4&bathrooms=2&storeys=2&garages=2&min_price=300000&max_price=500000 - universal search
+ * http://localhost:8000/api/properties/attribute?attribute=name&value=Geneva - search by name attribute
+ * http://localhost:8000/api/properties/attribute?attribute=bedrooms&value=5 - search by bedroom attribute
+ * http://localhost:8000/api/properties/attribute?attribute=bathrooms&value=3 - search by bathroom attribute
+ * http://localhost:8000/api/properties/attribute?attribute=storeys&value=1 - search by storey attribute
+ * http://localhost:8000/api/properties/attribute?attribute=garages&value=1 - search by garage attribute
+ * http://localhost:8000/api/properties/price/100000-300000 - search by price range
+ */
