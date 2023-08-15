4cd6d81453f3ff4518af0f538914a2d460efdf9b
Larionov-Yurii <larionov.yurii.work@gmail.com>
2023-08-15 22:03:01 +0300

    Updated vite.config.json file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: "resources/js/app.js",
             refresh: true,
         }),
     ],
+    server: {
+        proxy: {
+            "/api": "http://localhost",
+        },
+    },
 });
